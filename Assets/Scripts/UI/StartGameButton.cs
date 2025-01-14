281ac4a9e08c56b78d7e02c553f1d80d03d3c1a4
Hongxin Li <vidorxin@gmail.com>
2025-01-14 16:32:32 -0500

    level+icon update

--- Assets/Scripts/UI/StartGameButton.cs
+++ Assets/Scripts/UI/StartGameButton.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class StartGameButton : MonoBehaviour
+public class StartGameButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public string LevelScene;
 
+    private Vector3 originalPosition; // Store the original position of the button
+    public float hoverOffset = 10f; // Offset to move the button up on hover
+
+    void Start()
+    {
+        // Save the original position of the button
+        originalPosition = transform.position;
+    }
+
     public void LoadScene()
     {
-        
-            SceneManager.LoadScene(LevelScene);
-       
+        SceneManager.LoadScene(LevelScene);
+    }
+
+    public void ExitGame()
+    {
+        // Log message for testing in the Unity Editor
+        Debug.Log("Exiting game...");
+
+        // Close the application
+        Application.Quit();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Move the button up slightly when hovered over
+        transform.position = originalPosition + new Vector3(0, hoverOffset, 0);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Reset the button's position when the pointer exits
+        transform.position = originalPosition;
     }
 }
